bb225b6d57dce9981035e8f3aa8fb61bf59a8cf5
Trần Linh Thuận <linh.thuan.113@gmail.com>
2021-06-28 15:53:15 +0700

    Add project files.

---
+++ 1811062547_TranLinhThuan_bigschoool/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_1811062547_TranLinhThuan_bigschoool.MvcApplication" Language="C#" %>
---
+++ 1811062547_TranLinhThuan_bigschoool/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_1811062547_TranLinhThuan_bigschoool.Startup))]
+namespace _1811062547_TranLinhThuan_bigschoool
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
